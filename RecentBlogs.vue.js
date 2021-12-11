67225d2156f6ab81b6f9019bcbb7f9d021cd2c66
Arthur Judd <amjudd315@gmail.com>
2021-12-11 13:12:57 -0700

    moved stylesheet and template inside component

--- RecentBlogs.vue.js
+++ RecentBlogs.vue.js
@@ -1,70 +1,24 @@
-const stylesheet = /*html*/`
-<style>
-    .strb-main  {
-        display: flex;
-        flex-wrap: wrap;
-    }
-    .strb-blog-wrapper {
-        flex-grow: 1;
-        display: flex;
-        justify-content: center;
-    }
-    .strb-blog-container {
-        margin: 1em;
-        width:100%;
-        max-width: 20em;
-        min-width: 10em;
-        border: 1px solid #ccc;
-    }
-    .strb-thumb-wrapper {
-        width:100%;
-        height: 8em;
-        display: flex"
-    }
-    .strb-thumb {
-        object-fit: cover;
-        height: 100%;
-        width: 100%;
-        object-position: top;
-    }
-    .strb-blog-content {
-        padding:1em;
-    }
-
-    .strb-blog-title {
-    }
-    
-    .strb-blog-blurb {
-        word-break: break-word;
-    }
-    
-    .strb-button-wrapper {}
-</style>
-`
-
-const template = /*html*/`
-<div id="RecentBlogsVueWrapper">
-    <div v-html="stylesheet"></div>
-    
-    <div v-if="loading" class="strb-loading">Loading...</div>
-    <div v-else class="strb-main">
-        <div v-for="blog in recents" class="strb-blog-wrapper">
-            <div class="strb-blog-container">
-                <div class="strb-thumb-wrapper" v-if="showThumbs">
-                    <img class="strb-thumb" :src="blog.image" />
-                </div>
-                <div class="strb-blog-content">
-                    <div class="strb-blog-title">{{blog.title || "No Title"}}</div>
-                    <div class="strb-blog-blurb">{{blog.description}}</div>    
-                    <div class="strb-button-wrapper"><a class="btn btn-primary" :href="blog.st_link">Read More</a></div>
-                </div>
-            </div>
-        </div>    
-    </div>
-</div>`
-
 Vue.component('recentblogs',{
-    template,
+    template: /*html*/`
+    <div id="RecentBlogsVueWrapper">
+        <div v-html="stylesheet"></div>
+        
+        <div v-if="loading" class="strb-loading">Loading...</div>
+        <div v-else class="strb-main">
+            <div v-for="blog in recents" class="strb-blog-wrapper">
+                <div class="strb-blog-container">
+                    <div class="strb-thumb-wrapper" v-if="showThumbs">
+                        <img class="strb-thumb" :src="blog.image" />
+                    </div>
+                    <div class="strb-blog-content">
+                        <div class="strb-blog-title">{{blog.title || "No Title"}}</div>
+                        <div class="strb-blog-blurb">{{blog.description}}</div>    
+                        <div class="strb-button-wrapper"><a class="btn btn-primary" :href="blog.st_link">Read More</a></div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </div>`,
 
     data: function() {
         return {
@@ -73,7 +27,50 @@ Vue.component('recentblogs',{
             showThumbs: this.config.SHOW_THUMBNAILS,
             unusedDefaultThumbs: [],
             loading: true,
-            stylesheet
+            
+            stylesheet: /*html*/`
+            <style>
+                .strb-main  {
+                    display: flex;
+                    flex-wrap: wrap;
+                }
+                .strb-blog-wrapper {
+                    flex-grow: 1;
+                    display: flex;
+                    justify-content: center;
+                }
+                .strb-blog-container {
+                    margin: 1em;
+                    width:100%;
+                    max-width: 20em;
+                    min-width: 10em;
+                    border: 1px solid #ccc;
+                }
+                .strb-thumb-wrapper {
+                    width:100%;
+                    height: 8em;
+                    display: flex"
+                }
+                .strb-thumb {
+                    object-fit: cover;
+                    height: 100%;
+                    width: 100%;
+                    object-position: top;
+                }
+                .strb-blog-content {
+                    padding:1em;
+                }
+            
+                .strb-blog-title {
+                }
+                
+                .strb-blog-blurb {
+                    word-break: break-word;
+                }
+                
+                .strb-button-wrapper {}
+            </style>
+            `
         }
     },
 
